561d2c0b6c3fee5fe4d1ce2ba368199f7936e5e7
alexmcgil <alexmcgil@vivaldi.net>
2023-01-26 20:14:13 +0300

    404 status + text "User not found"

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/user/UserResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/user/UserResourceController.java
@@ -1,11 +1,15 @@
-package com.javamentor.qa.platform.webapp.controllers.rest;
+package com.javamentor.qa.platform.webapp.controllers.rest.user;
 
+import com.javamentor.qa.platform.models.dto.user.UserDto;
 import com.javamentor.qa.platform.service.abstracts.dto.user.UserDtoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
@@ -19,6 +23,14 @@ public class UserResourceController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getUser(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(userDtoService.getById(id));
+        Optional<UserDto> userDtoOptional = userDtoService.getById(id);
+
+        if (userDtoOptional.isPresent()) {
+            UserDto userDto = userDtoOptional.get();
+            return ResponseEntity.ok(userDto);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("User with id " + id + " not found");
+        }
     }
 }
