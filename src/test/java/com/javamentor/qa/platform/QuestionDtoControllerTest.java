bceb1ec23f28e232ec829a58140de25ccacbc7cd
JeRomeGrill <glushko.r@gmail.com>
2023-01-27 12:47:06 +0300

    test fixed

--- src/test/java/com/javamentor/qa/platform/QuestionDtoControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/QuestionDtoControllerTest.java
@@ -1,31 +1,15 @@
 package com.javamentor.qa.platform;
 
-import com.github.database.rider.core.api.configuration.DBUnit;
-import com.github.database.rider.junit5.api.DBRider;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.javamentor.qa.platform.webapp.configs.JmApplication;
 import org.junit.jupiter.api.Test;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import java.util.Random;
 
-
-@DBRider
-@DBUnit(caseSensitiveTableNames = true, cacheConnection = false, allowEmptyFields = true)
-@SpringBootTest(classes = JmApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@ActiveProfiles("test")
-@TestPropertySource(locations = "classpath:application-test.properties")
-@AutoConfigureMockMvc
 class QuestionDtoControllerTest extends AbstractTestApi {
 
 
@@ -35,7 +19,7 @@ class QuestionDtoControllerTest extends AbstractTestApi {
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
         this.mvc.perform(
-                get("/api/user/question/{id}", id))
+                        get("/api/user/question/{id}", id))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
