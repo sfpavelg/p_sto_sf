4d6cd00a0161cc870c9e73fcaa3f19a246ae387b
alexmcgil <alexmcgil@vivaldi.net>
2023-01-28 18:21:40 +0300

    Rewrite integrated test to MockMvc, added execution phases with sql script to remove data after test.

---
+++ src/test/java/com/javamentor/qa/platform/api/TestUserResourceController.java
@@ -0,0 +1,74 @@
+package com.javamentor.qa.platform.api;
+
+import com.javamentor.qa.platform.AbstractTestApi;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+public class TestUserResourceController extends AbstractTestApi {
+
+    @Test
+    @SqlGroup({
+            @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+                    value = {"/script/test-userDto-getId-withReputation-data.sql"}),
+            @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
+                    value = {"/script/test-userDto-getId-withReputation-data-remove.sql"})
+    })
+    public void testGetUserById() throws Exception {
+//        Successfully test's
+        mvc.perform(get("/api/user/{id}", 100))
+                .andExpect(jsonPath("$.id", Is.is(100)))
+                .andExpect(jsonPath("$.email", Is.is("email1@domain.com")))
+                .andExpect(jsonPath("$.fullName", Is.is("name1")))
+                .andExpect(jsonPath("$.city", Is.is("moscow")))
+                .andExpect(jsonPath("$.imageLink", Is.is("http://imagelink1.com")))
+                .andExpect(jsonPath("$.reputation", Is.is(6)));
+
+        mvc.perform(get("/api/user/{id}", 101))
+                .andExpect(jsonPath("$.id", Is.is(101)))
+                .andExpect(jsonPath("$.email", Is.is("email2@domain.com")))
+                .andExpect(jsonPath("$.fullName", Is.is("name2")))
+                .andExpect(jsonPath("$.city", Is.is("spb")))
+                .andExpect(jsonPath("$.imageLink", Is.is("http://imagelink2.com")))
+                .andExpect(jsonPath("$.reputation", Is.is(14)));
+
+        mvc.perform(get("/api/user/{id}", 102))
+                .andExpect(jsonPath("$.id", Is.is(102)))
+                .andExpect(jsonPath("$.email", Is.is("email3@domain.com")))
+                .andExpect(jsonPath("$.fullName", Is.is("name3")))
+                .andExpect(jsonPath("$.city", Is.is("NY")))
+                .andExpect(jsonPath("$.imageLink", Is.is("http://imagelink3.com")))
+                .andExpect(jsonPath("$.reputation", Is.is(11)));
+
+        mvc.perform(get("/api/user/{id}", 103))
+                .andExpect(jsonPath("$.id", Is.is(103)))
+                .andExpect(jsonPath("$.email", Is.is("email4@domain.com")))
+                .andExpect(jsonPath("$.fullName", Is.is("name4")))
+                .andExpect(jsonPath("$.city", Is.is("spb")))
+                .andExpect(jsonPath("$.imageLink", Is.is("http://imagelink4.com")))
+                .andExpect(jsonPath("$.reputation", Is.is(8)));
+
+//        Successfully test's for user without reputation
+        mvc.perform(get("/api/user/{id}", 104))
+                .andExpect(jsonPath("$.id", Is.is(104)))
+                .andExpect(jsonPath("$.email", Is.is("email5@domain.com")))
+                .andExpect(jsonPath("$.fullName", Is.is("name5")))
+                .andExpect(jsonPath("$.city", Is.is("moscow")))
+                .andExpect(jsonPath("$.imageLink", Is.is("http://imagelink5.com")))
+                .andExpect(jsonPath("$.reputation", Is.is(0)));
+
+//        By not existing userId test
+        mvc.perform(get("/api/user/{id}", 150))
+                .andExpect(status().isNotFound());
+
+    }
+
+}
---
+++ src/test/resources/script/test-userDto-getId-withReputation-data-remove.sql
@@ -0,0 +1,21 @@
+DELETE FROM "reputation" WHERE id = 100;
+DELETE FROM "reputation" WHERE id = 101;
+DELETE FROM "reputation" WHERE id = 102;
+DELETE FROM "reputation" WHERE id = 103;
+DELETE FROM "reputation" WHERE id = 104;
+DELETE FROM "reputation" WHERE id = 105;
+DELETE FROM "reputation" WHERE id = 106;
+DELETE FROM "reputation" WHERE id = 107;
+DELETE FROM "reputation" WHERE id = 108;
+DELETE FROM "reputation" WHERE id = 109;
+DELETE FROM "reputation" WHERE id = 110;
+DELETE FROM "reputation" WHERE id = 111;
+
+DELETE FROM "user_entity" WHERE id = 100;
+DELETE FROM "user_entity" WHERE id = 101;
+DELETE FROM "user_entity" WHERE id = 102;
+DELETE FROM "user_entity" WHERE id = 103;
+DELETE FROM "user_entity" WHERE id = 104;
+
+DELETE FROM "role" WHERE id = 1;
+DELETE FROM "role" WHERE id = 2;
