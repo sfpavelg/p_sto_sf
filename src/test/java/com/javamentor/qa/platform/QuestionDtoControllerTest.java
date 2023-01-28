5f43f0cf6f37e8a7705f2552cf1f8b7ad6602718
JeRomeGrill <glushko.r@gmail.com>
2023-01-28 14:36:08 +0300

    before and after sql scripts were added

--- src/test/java/com/javamentor/qa/platform/QuestionDtoControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/QuestionDtoControllerTest.java
@@ -2,27 +2,37 @@ package com.javamentor.qa.platform;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.util.Random;
-@Sql ({"/question-dto-data.sql"})
+
 class QuestionDtoControllerTest extends AbstractTestApi {
 
 
-    private int id = new Random().nextInt(5);
-
-
     @Test
-    public void shouldReturnDefaultMessage() throws Exception {
-        this.mvc.perform(
-                        get("/api/user/question/{id}", id))
+    @Sql (value = {"/question/question-dto-data-create.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql (value = {"/question/question-dto-data-drop.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void getQuestionDtoByIdTest() throws Exception {
+        //success
+        this.mvc.perform(get("/api/user/question/{id}", 1))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", Is.is(1)))
+                .andExpect(jsonPath("$.title", Is.is("title1")));
     }
+
+//    @Test
+//    @Sql (value = {"/question/question-dto-data-create.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+//    public void shouldReturnDefaultMessage2() throws Exception {
+//        this.mvc.perform(get("/api/user/question/{id}", 2))
+//                .andDo(print())
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+//    }
 }
