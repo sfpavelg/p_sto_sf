db86c2cf371430f958475e4d525c1b3e828a9f8e
alexmcgil <alexmcgil@vivaldi.net>
2023-01-30 20:03:40 +0300

    Added annotation AllArgsConstructor

--- src/main/java/com/javamentor/qa/platform/models/dto/PageDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/PageDto.java
@@ -1,5 +1,6 @@
 package com.javamentor.qa.platform.models.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PageDto<T> {
     private int currentPageNumber;
     private int totalPageCount;
