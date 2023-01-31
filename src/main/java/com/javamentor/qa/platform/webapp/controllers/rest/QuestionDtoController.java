137fa01c529793fc57ba8dd69c9a5c84e2550801
JeRomeGrill <glushko.r@gmail.com>
2023-01-31 12:49:54 +0300

    Description of controller added through swagger (without swagger config)

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionDtoController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionDtoController.java
@@ -2,6 +2,8 @@ package com.javamentor.qa.platform.webapp.controllers.rest;
 
 import com.javamentor.qa.platform.models.dto.question.QuestionDto;
 import com.javamentor.qa.platform.service.abstracts.dto.question.QuestionDtoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user/question")
+@Api("Контроллер для работы с QuestionDto")
 public class QuestionDtoController {
 
     private final QuestionDtoService questionDtoService;
@@ -19,6 +22,7 @@ public class QuestionDtoController {
 
 
     @GetMapping("/{id}")
+    @ApiOperation("Получение элемента QuestionDto по id")
     public ResponseEntity<?> getQuestionDtoById(@PathVariable Long id) {
         QuestionDto questionDto = questionDtoService.getQuestionDtoById(id).orElse(null);
         return questionDto == null ?
