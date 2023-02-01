9871c9590893b16d9ed700fb1ccd1f2a07ea096c
JeRomeGrill <glushko.r@gmail.com>
2023-02-01 14:32:14 +0300

    fix: optional deleted

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/tag/TagDtoDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/tag/TagDtoDao.java
@@ -3,8 +3,7 @@ package com.javamentor.qa.platform.dao.abstracts.dto.tag;
 import com.javamentor.qa.platform.models.dto.tag.TagDto;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TagDtoDao {
-    Optional<List<TagDto>> getTagDtoById(Long id);
+    List<TagDto> getTagDtoById(Long id);
 }
--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/tag/TagDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/tag/TagDtoDaoImpl.java
@@ -2,16 +2,13 @@ package com.javamentor.qa.platform.dao.impl.dto.tag;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.tag.TagDtoDao;
 import com.javamentor.qa.platform.models.dto.tag.TagDto;
-import com.javamentor.qa.platform.models.entity.question.Tag;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import javax.persistence.Query;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @Repository
 public class TagDtoDaoImpl implements TagDtoDao {
@@ -19,20 +16,14 @@ public class TagDtoDaoImpl implements TagDtoDao {
     @PersistenceContext
     private EntityManager entityManager;
 
-    private final ModelMapper modelMapper = new ModelMapper();
-
-
     @Override
-    public Optional<List<TagDto>> getTagDtoById(Long id) {
-        TypedQuery<Tag> query = entityManager.
-                createQuery("select t " +
-                                "from Tag t join t.questions as tq where tq.id = :id",
-                        Tag.class).
-                setParameter("id", id);
-        List<Tag> tagList = query.getResultList();
-        List<TagDto> tagDtoList = tagList.stream()
-                .map(tag -> modelMapper.map(tag, TagDto.class))
-                .collect(Collectors.toList());
-        return Optional.of(tagDtoList);
+    public List<TagDto> getTagDtoById(Long id) {
+        Query query = entityManager.createQuery("select new com.javamentor.qa.platform.models.dto.tag.TagDto(" +
+                        "t.id, " +
+                        "t.name, " +
+                        "t.description) " +
+                        "from Tag t join t.questions as tq where tq.id = :id")
+                .setParameter("id", id);
+        return (List<TagDto>) query.getResultList();
     }
 }
