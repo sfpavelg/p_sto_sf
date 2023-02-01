3fc307525d11960e4d4db1f529526b672c882cd6
JeRomeGrill <glushko.r@gmail.com>
2023-02-01 14:24:15 +0300

    feat: NotFoundException Handler created

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/ExceptionController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/ExceptionController.java
@@ -1,5 +1,6 @@
 package com.javamentor.qa.platform.webapp.controllers;
 
+import javassist.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -25,5 +26,12 @@ public class ExceptionController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(exception.getMessage());
      }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
 
 }
