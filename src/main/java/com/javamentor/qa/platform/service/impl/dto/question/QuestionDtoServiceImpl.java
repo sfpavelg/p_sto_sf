9824e301af0d9915bf66e06371b64493f00db597
JeRomeGrill <glushko.r@gmail.com>
2023-02-01 14:31:49 +0300

    fix: null check changed, exception handler added

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/question/QuestionDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/question/QuestionDtoServiceImpl.java
@@ -1,32 +1,34 @@
 package com.javamentor.qa.platform.service.impl.dto.question;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.question.QuestionDtoDao;
+import com.javamentor.qa.platform.dao.abstracts.dto.tag.TagDtoDao;
 import com.javamentor.qa.platform.models.dto.question.QuestionDto;
 import com.javamentor.qa.platform.service.abstracts.dto.question.QuestionDtoService;
-import com.javamentor.qa.platform.service.abstracts.dto.tag.TagDtoService;
+import javassist.NotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Optional;
 
 @Service
 public class QuestionDtoServiceImpl implements QuestionDtoService {
 
     private final QuestionDtoDao questionDtoDao;
-    private final TagDtoService tagDtoService;
+    private final TagDtoDao tagDtoDao;
 
-    public QuestionDtoServiceImpl(QuestionDtoDao questionDtoDao, TagDtoService tagDtoService) {
+    public QuestionDtoServiceImpl(QuestionDtoDao questionDtoDao, TagDtoDao tagDtoDao) {
         this.questionDtoDao = questionDtoDao;
-        this.tagDtoService = tagDtoService;
+        this.tagDtoDao = tagDtoDao;
     }
 
 
     @Override
-    public Optional<QuestionDto> getQuestionDtoById(Long id) {
-        QuestionDto questionDto = questionDtoDao.getQuestionDtoById(id).orElse(null);
-        if (questionDto != null) {
-            questionDto.setListTagDto(tagDtoService.getTagDtoById(id).orElse(new ArrayList<>()));
+    public QuestionDto getQuestionDtoById(Long id) throws NotFoundException {
+
+        if (questionDtoDao.getQuestionDtoById(id).isPresent()) {
+            QuestionDto questionDto = questionDtoDao.getQuestionDtoById(id).get();
+            questionDto.setListTagDto(tagDtoDao.getTagDtoById(id));
+            return questionDto;
+        } else {
+            throw new NotFoundException("QuestionDto with id = " + id + " not found");
         }
-        return Optional.ofNullable(questionDto);
     }
 }
