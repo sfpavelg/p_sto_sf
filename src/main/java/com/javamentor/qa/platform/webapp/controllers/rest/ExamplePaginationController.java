ff87587e3bf68ab48c533013368a5893f5dd4891
alexmcgil <alexmcgil@vivaldi.net>
2023-02-02 22:58:19 +0300

    feat: example pagination controller with documentation

---
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ExamplePaginationController.java
@@ -0,0 +1,72 @@
+package com.javamentor.qa.platform.webapp.controllers.rest;
+
+import com.javamentor.qa.platform.models.dto.ExampleDto;
+import com.javamentor.qa.platform.service.abstracts.dto.ExampleDtoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/example")
+@Api(value = "Example pagination controller")
+public class ExamplePaginationController {
+
+    private final ExampleDtoService exampleDtoService;
+
+    public ExamplePaginationController(ExampleDtoService exampleDtoService) {
+        this.exampleDtoService = exampleDtoService;
+    }
+
+    @GetMapping("/pagination")
+    @ApiOperation(value = "Get example of paginated Dto",
+            notes = "Available parameters and their values:" +
+                    "1. pageNumber is a number of page with dto's." +
+                    "2. itemsOnPage is a number that means the number of elements on the page")
+    public ResponseEntity<?> getAllUsers(@RequestParam(defaultValue = "0") int pageNumber,
+                                         @RequestParam(defaultValue = "10") int itemsOnPage) {
+        HashMap<String, Object> param = new HashMap<>();
+        param.put("pageNumber", pageNumber);
+        param.put("itemsOnPage", itemsOnPage);
+        // For sortBy default value is "id" sorting/
+        param.put("sortBy", "id");
+
+        List<ExampleDto> userPage = exampleDtoService.getListingUsers(param);
+
+        if (!userPage.isEmpty()) {
+            return ResponseEntity.ok(userPage);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Page with param " + param + " not found");
+        }
+    }
+
+    @GetMapping("/anotherPagination")
+    @ApiOperation(value = "Get example of another paginated Dto",
+            notes = "Available parameters and their values:" +
+                    "1. pageNumber is a number of page with dto's." +
+                    "2. sortBy is the value of a string which in this example" +
+                    " can be id, email, fullName, city, imageLink, or reputation")
+    public ResponseEntity<?> getAnotherUsers(@RequestParam(defaultValue = "0") int pageNumber,
+                                             @RequestParam(defaultValue = "id") String sortBy) {
+        HashMap<String, Object> param = new HashMap<>();
+        param.put("pageNumber", pageNumber);
+        param.put("sortBy", sortBy);
+
+        List<ExampleDto> userPage = exampleDtoService.getAnotherListingUsers(param);
+
+        if (!userPage.isEmpty()) {
+            return ResponseEntity.ok(userPage);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Page with param " + param + " not found");
+        }
+    }
+}
