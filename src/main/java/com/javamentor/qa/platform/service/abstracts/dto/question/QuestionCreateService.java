69f139b7fc17069e578a6e4883825dd40c6087b0
Сергей Андреевич Токарев <tokarev@niuitmo.ru>
2023-02-03 16:48:38 +0400

    fix: refactor sql init script

--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/question/QuestionCreateService.java
+++
@@ -1,8 +0,0 @@
-package com.javamentor.qa.platform.service.abstracts.dto.question;
-
-import com.javamentor.qa.platform.models.dto.question.QuestionCreateDto;
-import com.javamentor.qa.platform.models.entity.question.Question;
-import com.javamentor.qa.platform.service.abstracts.model.ReadWriteService;
-
-public interface QuestionCreateService extends ReadWriteService <Question, Long> {
-}
