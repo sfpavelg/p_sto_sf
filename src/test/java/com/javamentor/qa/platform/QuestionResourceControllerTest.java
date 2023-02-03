93bbbb75f4e289bb6f6b41e9883202cb1dfef6da
JeRomeGrill <glushko.r@gmail.com>
2023-02-03 14:36:41 +0300

    feat: success test added

--- src/test/java/com/javamentor/qa/platform/QuestionResourceControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/QuestionResourceControllerTest.java
@@ -7,7 +7,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.javamentor.qa.platform.models.dto.question.QuestionCreateDto;
 import com.javamentor.qa.platform.models.dto.tag.TagDto;
-import com.javamentor.qa.platform.models.entity.question.Tag;
 import org.hamcrest.core.Is;
 import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.Test;
@@ -22,8 +21,8 @@ class QuestionResourceControllerTest extends AbstractTestApi {
 
 
     @Test
-    @Sql(value = {"/question/question-dto-data-create.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(value = {"/question/question-dto-data-drop.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql(value = {"/script/question/getQuestionDtoByIdTest/question-dto-data-create.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = {"/script/question/getQuestionDtoByIdTest/question-dto-data-drop.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     public void getQuestionDtoByIdTest() throws Exception {
         //success
         this.mvc.perform(get("/api/user/question/{id}", 1))
@@ -79,20 +78,31 @@ class QuestionResourceControllerTest extends AbstractTestApi {
 
 
     @Test
-    @Sql(value = {"/question/question-add-data-create.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-//    @Sql(value = {"/question/question-add-data-drop.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Sql(value = {"/script/question/addQuestionTest/question-add-data-create.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = {"/script/question/addQuestionTest/question-add-data-drop.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     public void addQuestionTest() throws Exception {
-        List<TagDto> list = new ArrayList<>();
-        list.add(new TagDto(1L,"name1","description1"));
-        QuestionCreateDto questionCreateDto = new QuestionCreateDto();
-        questionCreateDto.setDescription("testDescription");
-        questionCreateDto.setTags(list);
-        questionCreateDto.setTitle("testTitle");
-        this.mvc.perform(post("/api/user/question").content(this.objectMapper.writeValueAsString(questionCreateDto))
+        List<TagDto> list1 = new ArrayList<>();
+        list1.add(new TagDto(null,"name1",null));
+        this.mvc.perform(post("/api/user/question").content(this.objectMapper.writeValueAsString(
+                new QuestionCreateDto("testTitle1", "testDescription1", list1)))
                         .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", Is.is(1)))
+                .andExpect(jsonPath("$.title", Is.is("testTitle1")))
+                .andExpect(jsonPath("$.authorId", Is.is(100)))
+                .andExpect(jsonPath("$.authorReputation", Is.is(6)))
+                .andExpect(jsonPath("$.authorName", Is.is("name1")))
+                .andExpect(jsonPath("$.authorImage", Is.is("http://imagelink1.com")))
+                .andExpect(jsonPath("$.description", Is.is("testDescription1")))
+                .andExpect(jsonPath("$.viewCount", Is.is(0)))
+                .andExpect(jsonPath("$.countAnswer", Is.is(0)))
+                .andExpect(jsonPath("$.countValuable", Is.is(0)))
+                .andExpect(jsonPath("$.listTagDto[0].id", Is.is(100)))
+                .andExpect(jsonPath("$.listTagDto[0].name", Is.is("name1")))
+                .andExpect(jsonPath("$.listTagDto[0].description", Is.is("description1")))
+        ;
     }
 
 }
