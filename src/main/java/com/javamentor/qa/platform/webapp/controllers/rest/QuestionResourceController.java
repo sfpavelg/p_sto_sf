658521c6de10f2fa3ea4e6cd3a3823e291866746
JeRomeGrill <glushko.r@gmail.com>
2023-02-05 13:13:41 +0300

    fix: status description added for getQuestionDtoById method

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
@@ -1,8 +1,11 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
+import com.javamentor.qa.platform.models.dto.question.QuestionDto;
 import com.javamentor.qa.platform.service.abstracts.dto.question.QuestionDtoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import javassist.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,9 +22,13 @@ public class QuestionResourceController {
         this.questionDtoService = questionDtoService;
     }
 
-
     @GetMapping("/{id}")
-    @ApiOperation("Получение элемента QuestionDto по id")
+    @ApiOperation(value = "Получение элемента QuestionDto по id", response = QuestionDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success request. QuestionDto object returned in response"),
+            @ApiResponse(code = 401, message = "Unauthorized request"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Question with such id doesn't exist")})
     public ResponseEntity<?> getQuestionDtoById(@PathVariable Long id) throws NotFoundException {
         return ResponseEntity.ok(questionDtoService.getQuestionDtoById(id));
     }
