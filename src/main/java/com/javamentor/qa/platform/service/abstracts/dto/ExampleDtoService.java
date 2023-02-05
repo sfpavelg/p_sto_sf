d7008bf344a622b661dad03546ae313f293a78d4
alexmcgil <alexmcgil@vivaldi.net>
2023-02-05 21:47:00 +0300

    fix: removed unnecessary @Service annotation

--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/ExampleDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/dto/ExampleDtoService.java
@@ -1,12 +1,10 @@
 package com.javamentor.qa.platform.service.abstracts.dto;
 
 import com.javamentor.qa.platform.models.dto.ExampleDto;
-import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.List;
 
-@Service
 public interface ExampleDtoService {
     List<ExampleDto> getListingUsers(HashMap<String, Object> param);
 
