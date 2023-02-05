351606704f9ac8d884d4f300c21f3286494fa3e5
alexmcgil <alexmcgil@vivaldi.net>
2023-02-05 21:45:20 +0300

    fix: removed the logic of page assembly, separating the DAO layer from the Service layer

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/ExampleDtoDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/ExampleDtoDao.java
@@ -2,18 +2,10 @@ package com.javamentor.qa.platform.dao.abstracts.dto;
 
 import com.javamentor.qa.platform.models.dto.ExampleDto;
 import com.javamentor.qa.platform.models.entity.user.User;
-import org.springframework.stereotype.Repository;
-
-import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
 /**
  * A DTO for the {@link User} entity
  */
-@Repository
-public interface ExampleDtoDao extends PageDtoDao<ExampleDto, HashMap<String, Object>> {
-
-    List<ExampleDto> getPaginatedUsers(HashMap<String, Object> param);
-    List<ExampleDto> getAnotherPaginatedUsers(HashMap<String, Object> param);
-
+public interface ExampleDtoDao extends PageDtoDao<ExampleDto, Map<?, ?>> {
 }
--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/PageDtoDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/PageDtoDao.java
@@ -1,62 +1,32 @@
 package com.javamentor.qa.platform.dao.abstracts.dto;
 
-import com.javamentor.qa.platform.models.dto.PageDto;
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.Query;
 import java.util.List;
 
 /**
  * Universal pagination for DTO's
- * @param <T> The type of DTO for which we perform pagination
- * @param <HashMap> Parameter for page selection
+ *
+ * @param <T>   The type of DTO for which we perform pagination
+ * @param <Map> Parameters for page selection
  */
 
-@Repository
-public interface PageDtoDao<T, HashMap> {
+public interface PageDtoDao<T, Map> {
 
     /**
-     * Creating a query to get a list of DTO for it
+     * Get a list of DTO for creating PageDto and returning it on request
+     *
+     * @param param Is a parameters of pagination, such a "itemsOnPage", "currentPageNumber", etc.
+     * @return Paginated List of DTO's
      */
-    Query getItems(HashMap param);
+    List<T> getItems(Map param);
 
     /**
      * Get the total number of values for this DTO
+     *
+     * @param param Is a parameters of pagination, such a "itemsOnPage", "currentPageNumber", etc. In this method
+     *              it is possible to use values that limit the total number of DTO's.
+     * @return Total number of available DTOs to display
      */
-    int getTotalResultCount(HashMap param);
+    int getTotalResultCount(Map param);
 
-    /**
-     * Page constructor, by default, considers elements on page 10
-     * @param param By default, the parameter is the page number
-     * @param query The request to be paginated
-     * @param itemsOnPage Count items on page
-     * @param pageNumber Number of page that contains DTO's
-     * @return Returns a paginated object for the DTO
-     */
-    default PageDto<T> createPage(HashMap param, Query query, int itemsOnPage, int pageNumber) {
-
-        PageDto<T> page = new PageDto<>();
-        page.setTotalResultCount(getTotalResultCount(param));
-        page.setItemsOnPage(itemsOnPage);
-        page.setTotalPageCount(page.getTotalResultCount() / page.getItemsOnPage());
-        page.setCurrentPageNumber(pageNumber);
-        page.setItems(paginateQuery(query, page));
-
-        return page;
-    }
-
-    /**
-     * Query constructor to build List<DTO>
-     * @param query Query  to be paginated
-     * @param page The object of the page from which the pagination parameters are taken
-     * @return List of DTO's
-     */
-    default List<T> paginateQuery(Query query, PageDto<T> page) {
-        return query
-                .setMaxResults(page.getItemsOnPage())
-                .setFirstResult(page.getCurrentPageNumber() * page.getItemsOnPage())
-                .getResultList();
-
-    }
 
 }
