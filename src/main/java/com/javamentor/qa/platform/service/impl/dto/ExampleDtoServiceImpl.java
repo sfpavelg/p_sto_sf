2d99a5e2b404721ef840935e11ca063dedb802aa
alexmcgil <alexmcgil@vivaldi.net>
2023-02-05 21:52:18 +0300

    fix: an example using a new service and implementing a different pagination logic

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/ExampleDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/ExampleDtoServiceImpl.java
@@ -1,15 +1,19 @@
 package com.javamentor.qa.platform.service.impl.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.ExampleDtoDao;
+import com.javamentor.qa.platform.dao.abstracts.dto.PageDtoDao;
 import com.javamentor.qa.platform.models.dto.ExampleDto;
+import com.javamentor.qa.platform.models.dto.PageDto;
 import com.javamentor.qa.platform.service.abstracts.dto.ExampleDtoService;
+import com.javamentor.qa.platform.service.abstracts.dto.PageDtoService;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
-public class ExampleDtoServiceImpl implements ExampleDtoService {
+public class ExampleDtoServiceImpl extends PageDtoService<ExampleDto, Map<?, ?>> implements ExampleDtoService {
 
     private final ExampleDtoDao exampleDtoDao;
 
@@ -19,13 +23,28 @@ public class ExampleDtoServiceImpl implements ExampleDtoService {
 
     @Override
     public List<ExampleDto> getListingUsers(HashMap<String, Object> param) {
-        return exampleDtoDao.getPaginatedUsers(param);
+        PageDto<ExampleDto> page = createPage(param);
+        return page.getItems();
     }
 
     @Override
     public List<ExampleDto> getAnotherListingUsers(HashMap<String, Object> param) {
-        return exampleDtoDao.getAnotherPaginatedUsers(param);
+        PageDto<ExampleDto> page = createPage(param, exampleDtoDao);
+        return page.getItems();
     }
 
 
+    @Override
+    protected PageDto<ExampleDto> createPage(Map<?, ?> param, PageDtoDao<ExampleDto, Map<?, ?>> dao) {
+        PageDto<ExampleDto> page = new PageDto<>();
+        HashMap<String, Object> paramToMap = (HashMap<String, Object>) param;
+        paramToMap.put("itemsOnPage", 3);
+        page.setItemsOnPage((int) paramToMap.get("itemsOnPage"));
+        page.setTotalResultCount(exampleDtoDao.getTotalResultCount(paramToMap));
+        page.setTotalPageCount(page.getTotalResultCount() / page.getItemsOnPage());
+        page.setCurrentPageNumber((int) paramToMap.get("currentPageNumber"));
+        page.setItems(exampleDtoDao.getItems(paramToMap));
+
+        return page;
+    }
 }
