ff7f5a4a3d8f29c456d0593229f9934069114099
alexmcgil <alexmcgil@vivaldi.net>
2023-02-05 22:05:11 +0300

    fix: removed unnecessary annotation

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/user/UserDtoDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/user/UserDtoDao.java
@@ -2,14 +2,12 @@ package com.javamentor.qa.platform.dao.abstracts.dto.user;
 
 import com.javamentor.qa.platform.models.dto.user.UserDto;
 import com.javamentor.qa.platform.models.entity.user.User;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**
  * A DTO for the {@link User} entity
  */
-@Repository
 public interface UserDtoDao {
     Optional<UserDto> getById(Long id);
 }
