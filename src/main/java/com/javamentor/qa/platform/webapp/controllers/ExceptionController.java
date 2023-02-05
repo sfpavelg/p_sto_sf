aa0d16b7fcadb4741f76b26f39dbe84e2f4e9b97
JeRomeGrill <glushko.r@gmail.com>
2023-02-05 14:35:30 +0300

    feat: validation exception handler added

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/ExceptionController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/ExceptionController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -41,6 +42,13 @@ public class ExceptionController {
     public ResponseEntity<String> handleNullPointerException(NullPointerException exception){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(exception.getBindingResult().getFieldError().getDefaultMessage());
     }
 
 }
