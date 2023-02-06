4391f908595d9065faee335237e2785ba64c2ea5
alexmcgil <alexmcgil@vivaldi.net>
2023-02-06 19:18:29 +0300

    feat: NotFoundException in service layer

--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/user/UserDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/dto/user/UserDtoService.java
@@ -1,9 +1,10 @@
 package com.javamentor.qa.platform.service.abstracts.dto.user;
 
 import com.javamentor.qa.platform.models.dto.user.UserDto;
+import javassist.NotFoundException;
 
 import java.util.Optional;
 
 public interface UserDtoService {
-    Optional<UserDto> getById(Long id);
+    Optional<UserDto> getById(Long id) throws NotFoundException;
 }
--- src/main/java/com/javamentor/qa/platform/service/impl/dto/user/UserDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/user/UserDtoServiceImpl.java
@@ -3,6 +3,7 @@ package com.javamentor.qa.platform.service.impl.dto.user;
 import com.javamentor.qa.platform.dao.abstracts.dto.user.UserDtoDao;
 import com.javamentor.qa.platform.models.dto.user.UserDto;
 import com.javamentor.qa.platform.service.abstracts.dto.user.UserDtoService;
+import javassist.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,7 +18,11 @@ public class UserDtoServiceImpl implements UserDtoService {
     }
 
     @Override
-    public Optional<UserDto> getById(Long id) {
-        return userDtoDao.getById(id);
+    public Optional<UserDto> getById(Long id) throws NotFoundException {
+        if (userDtoDao.getById(id).isPresent()) {
+            return userDtoDao.getById(id);
+        }
+
+        throw new NotFoundException("User with id = " + id + " not found");
     }
 }
