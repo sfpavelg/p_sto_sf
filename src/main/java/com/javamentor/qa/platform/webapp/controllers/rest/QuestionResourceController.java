7a68c868aff4614295f71560f62387c703001ed0
JeRomeGrill <glushko.r@gmail.com>
2023-02-06 16:57:30 +0300

    feat: status codes added for addQuestion endpoint

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
@@ -55,7 +55,12 @@ public class QuestionResourceController {
     }
 
     @PostMapping
-    @ApiOperation("Получение элемента QuestionDto по id")
+    @ApiOperation(value = "Добавление вопроса. Ожидает заполненный объект QuestionCreateDto", response = QuestionDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success request. Question added to DB and it's QuestionDto object returned in response"),
+            @ApiResponse(code = 401, message = "Unauthorized request"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 400, message = "Validation failed. Fields of QuestionCreateDto must be not empty or null")})
     public ResponseEntity<?> addQuestion(@Valid @RequestBody QuestionCreateDto questionCreateDto) throws NotFoundException {
         Question question = mapper.map(questionCreateDto, Question.class);
         List<Tag> tagListDto = question.getTags();
