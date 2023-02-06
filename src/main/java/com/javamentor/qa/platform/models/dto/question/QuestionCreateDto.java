339e7811effd7354fe487b29d4d8610c87237786
JeRomeGrill <glushko.r@gmail.com>
2023-02-06 12:29:21 +0300

    refactor: excess import deleted

--- src/main/java/com/javamentor/qa/platform/models/dto/question/QuestionCreateDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/question/QuestionCreateDto.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 @Getter
