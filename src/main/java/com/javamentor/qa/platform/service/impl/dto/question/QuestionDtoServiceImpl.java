9fefd2bff523fef530ea9430049a2166a9c18cb9
JeRomeGrill <glushko.r@gmail.com>
2023-02-09 13:52:16 +0300

    feat: all logic moved to Service layer, solution is optimized

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/question/QuestionDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/question/QuestionDtoServiceImpl.java
@@ -2,12 +2,24 @@ package com.javamentor.qa.platform.service.impl.dto.question;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.question.QuestionDtoDao;
 import com.javamentor.qa.platform.dao.abstracts.dto.tag.TagDtoDao;
+import com.javamentor.qa.platform.dao.abstracts.model.QuestionDao;
+import com.javamentor.qa.platform.dao.abstracts.model.TagDao;
+import com.javamentor.qa.platform.dao.abstracts.model.UserDao;
+import com.javamentor.qa.platform.models.dto.question.QuestionCreateDto;
 import com.javamentor.qa.platform.models.dto.question.QuestionDto;
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.question.Tag;
+import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.dto.question.QuestionDtoService;
+import com.javamentor.qa.platform.webapp.converters.QuestionConverter;
 import javassist.NotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.security.Principal;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @Service
@@ -15,10 +27,21 @@ public class QuestionDtoServiceImpl implements QuestionDtoService {
 
     private final QuestionDtoDao questionDtoDao;
     private final TagDtoDao tagDtoDao;
+    private final QuestionDao questionDao;
 
-    public QuestionDtoServiceImpl(QuestionDtoDao questionDtoDao, TagDtoDao tagDtoDao) {
+    private final UserDao userDao;
+
+    private final TagDao tagDao;
+
+    private final QuestionConverter questionConverter;
+
+    public QuestionDtoServiceImpl(QuestionDtoDao questionDtoDao, TagDtoDao tagDtoDao, QuestionDao questionDao, UserDao userDao, TagDao tagDao, QuestionConverter questionConverter) {
         this.questionDtoDao = questionDtoDao;
         this.tagDtoDao = tagDtoDao;
+        this.questionDao = questionDao;
+        this.userDao = userDao;
+        this.tagDao = tagDao;
+        this.questionConverter = questionConverter;
     }
 
 
@@ -31,6 +54,30 @@ public class QuestionDtoServiceImpl implements QuestionDtoService {
             questionDto.setListTagDto(tagDtoDao.getTagDtoById(id));
             return questionDto;
         }
-            throw new NotFoundException("QuestionDto with id = " + id + " not found");
+        throw new NotFoundException("QuestionDto with id = " + id + " not found");
+    }
+
+    @Override
+    @Transactional
+    public QuestionDto addQuestion(QuestionCreateDto questionCreateDto, Principal principal) throws NotFoundException {
+        Question question = questionConverter.questionCreateDtoToQuestion(questionCreateDto);
+        List<Tag> tagListDto = question.getTags();
+        List<String> tagNamesList = new ArrayList<>();
+        tagListDto.forEach(tag -> tagNamesList.add(tag.getName()));
+        List<Tag> existsInDbTags = tagDao.getTagsByName(tagNamesList);
+        List<Tag> doesntExistTags = tagListDto.stream().filter(tag -> {
+            for (Tag t : existsInDbTags) {
+                if (t.getName().equals(tag.getName())) {
+                    return false;
+                }
+            }
+            return true;
+        }).collect(Collectors.toList());
+        tagDao.persistAll(doesntExistTags);
+        question.setTags(Stream.of(existsInDbTags, doesntExistTags).flatMap(Collection::stream).collect(Collectors.toList()));
+        Optional<User> user = userDao.getByEmail(principal.getName());
+        user.ifPresent(question::setUser);
+        questionDao.persist(question);
+        return getQuestionDtoById(question.getId());
     }
 }
