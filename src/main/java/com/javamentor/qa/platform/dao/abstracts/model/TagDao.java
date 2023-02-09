a8c532e91a9fe5db9b82c136e27dbd29870f2088
JeRomeGrill <glushko.r@gmail.com>
2023-02-09 13:55:46 +0300

    feat: new method added instead of previous

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/model/TagDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/model/TagDao.java
@@ -2,9 +2,9 @@ package com.javamentor.qa.platform.dao.abstracts.model;
 
 import com.javamentor.qa.platform.models.entity.question.Tag;
 
-import java.util.Map;
+import java.util.List;
 
 public interface TagDao extends ReadWriteDao<Tag, Long> {
 
-    Map<String, Long> getAllTagNamesAndIds();
+    List<Tag> getTagsByName(List<String> names);
 }
