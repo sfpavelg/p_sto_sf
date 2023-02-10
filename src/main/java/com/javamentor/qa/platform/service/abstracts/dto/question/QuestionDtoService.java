dc735bc0e18ff478e925a804e88794cf56286f45
JeRomeGrill <glushko.r@gmail.com>
2023-02-10 14:35:52 +0300

    refactor: addQuestion method moved to QuestionService

--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/question/QuestionDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/dto/question/QuestionDtoService.java
@@ -1,14 +1,10 @@
 package com.javamentor.qa.platform.service.abstracts.dto.question;
 
-import com.javamentor.qa.platform.models.dto.question.QuestionCreateDto;
 import com.javamentor.qa.platform.models.dto.question.QuestionDto;
 import javassist.NotFoundException;
-
-import java.security.Principal;
 
 
 public interface QuestionDtoService {
     QuestionDto getQuestionDtoById(Long id) throws NotFoundException;
 
-    QuestionDto addQuestion(QuestionCreateDto questionCreateDto, Principal principal) throws NotFoundException;
 }
