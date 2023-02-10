dd0d4aa1ef4da9d4b856d503526b6f8b4df11df1
JeRomeGrill <glushko.r@gmail.com>
2023-02-10 18:40:19 +0300

    refactor: User parameter removed from service

--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/QuestionService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/QuestionService.java
@@ -2,10 +2,9 @@ package com.javamentor.qa.platform.service.abstracts.model;
 
 import com.javamentor.qa.platform.models.dto.question.QuestionDto;
 import com.javamentor.qa.platform.models.entity.question.Question;
-import com.javamentor.qa.platform.models.entity.user.User;
 import javassist.NotFoundException;
 
 
 public interface QuestionService extends ReadWriteService<Question, Long> {
-    QuestionDto addQuestion(Question question, User user) throws NotFoundException;
+    QuestionDto addQuestion(Question question) throws NotFoundException;
 }
