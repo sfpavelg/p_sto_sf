648ab636488be91919268a421103c24b8b66c325
JeRomeGrill <glushko.r@gmail.com>
2023-02-11 12:15:21 +0300

    refactor: codestyle

--- src/main/java/com/javamentor/qa/platform/webapp/converters/QuestionConverter.java
+++ src/main/java/com/javamentor/qa/platform/webapp/converters/QuestionConverter.java
@@ -9,13 +9,13 @@ import org.mapstruct.Named;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-@Mapper (componentModel = "spring")
+@Mapper(componentModel = "spring")
 public abstract class QuestionConverter {
     @Mapping(target = "user", source = "user", qualifiedByName = "setUser")
     public abstract Question questionCreateDtoToQuestion(QuestionCreateDto questionCreateDto, User user);
 
     @Named("setUser")
-    public  User setUser (User  user) {
+    public User setUser(User user) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return (User) authentication.getPrincipal();
     }
