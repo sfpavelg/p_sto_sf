5ddd08753ffd173244421361f1cd01d9984fde9b
JeRomeGrill <glushko.r@gmail.com>
2023-02-11 12:16:06 +0300

    refactor: redundant method deleted

--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/QuestionService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/model/QuestionService.java
@@ -1,10 +1,7 @@
 package com.javamentor.qa.platform.service.abstracts.model;
 
-import com.javamentor.qa.platform.models.dto.question.QuestionDto;
 import com.javamentor.qa.platform.models.entity.question.Question;
-import javassist.NotFoundException;
 
 
 public interface QuestionService extends ReadWriteService<Question, Long> {
-    QuestionDto addQuestion(Question question) throws NotFoundException;
 }
