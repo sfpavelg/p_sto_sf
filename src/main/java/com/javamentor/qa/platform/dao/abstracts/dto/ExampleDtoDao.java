b379b06f653b1687074b9f866461d2e89dd79995
alexmcgil <alexmcgil@vivaldi.net>
2023-02-11 21:53:30 +0300

    fix: generics with correct definition

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/ExampleDtoDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/ExampleDtoDao.java
@@ -7,5 +7,5 @@ import java.util.Map;
 /**
  * A DTO for the {@link User} entity
  */
-public interface ExampleDtoDao extends PageDtoDao<ExampleDto, Map<?, ?>> {
+public interface ExampleDtoDao extends PageDtoDao<ExampleDto, Map<String, Object>> {
 }
