6f6aa4e045521d662bcc6b02c7e5c84749876ebe
alexmcgil <alexmcgil@vivaldi.net>
2023-02-11 21:57:13 +0300

    fix: start of logic configuring pagination for Dto's

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/ExamplePaginationPaginationDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/ExamplePaginationPaginationDtoServiceImpl.java
@@ -4,25 +4,44 @@ import com.javamentor.qa.platform.models.dto.ExampleDto;
 import com.javamentor.qa.platform.models.dto.PageDto;
 import com.javamentor.qa.platform.service.abstracts.dto.ExamplePaginationDtoService;
 import com.javamentor.qa.platform.service.abstracts.dto.PageDtoService;
+import javassist.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Service
-public class ExamplePaginationPaginationDtoServiceImpl extends PageDtoService<ExampleDto, Map<?, ?>> implements ExamplePaginationDtoService {
+public class ExamplePaginationPaginationDtoServiceImpl extends PageDtoService<ExampleDto, Map<String, Object>> implements ExamplePaginationDtoService {
 
     @Override
-    public List<ExampleDto> getListingUsers(HashMap<String, Object> param) {
+    public PageDto<ExampleDto> getListingUsers(HashMap<String, Object> param) throws NotFoundException {
+        param.put("itemsOnPage", 10);
+        param.put("DtoType", "ExampleDtoDao");
+        // For sortBy default value is "id" sorting/
+        param.put("sortBy", "id");
+
         PageDto<ExampleDto> page = createPage(param);
-        return page.getItems();
+
+        if (page.getItems().isEmpty()) {
+            throw new NotFoundException("Page with params not found");
+        }
+        return page;
     }
 
     @Override
-    public List<ExampleDto> getAnotherListingUsers(HashMap<String, Object> param) {
+    public PageDto<ExampleDto> getAnotherListingUsers(HashMap<String, Object> param) throws NotFoundException {
+        param.put("itemsOnPage", 10);
+        param.put("DtoType", "ExampleDtoDao");
+        param.put("sortBy", "id");
+        // Here we can define the class that will be used to call the methods
+        // TODO: create a REAL anotherListingUsers
+        param.put("DtoImpl", "exampleDtoDaoImpl");
         PageDto<ExampleDto> page = createPage(param);
-        return page.getItems();
+
+        if (page.getItems().isEmpty()) {
+            throw new NotFoundException("Page with params not found");
+        }
+        return page;
     }
 }
 
