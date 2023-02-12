70d86d8ccbf6c03271be06fabacde0847a292b4e
alexmcgil <alexmcgil@vivaldi.net>
2023-02-12 19:26:09 +0300

    fix: return type now is UserDto and refactored logic of Service

--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/user/UserDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/dto/user/UserDtoService.java
@@ -3,8 +3,6 @@ package com.javamentor.qa.platform.service.abstracts.dto.user;
 import com.javamentor.qa.platform.models.dto.user.UserDto;
 import javassist.NotFoundException;
 
-import java.util.Optional;
-
 public interface UserDtoService {
-    Optional<UserDto> getById(Long id) throws NotFoundException;
+    UserDto getById(Long id) throws NotFoundException;
 }
--- src/main/java/com/javamentor/qa/platform/service/impl/dto/user/UserDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/user/UserDtoServiceImpl.java
@@ -4,23 +4,22 @@ import com.javamentor.qa.platform.dao.abstracts.dto.user.UserDtoDao;
 import com.javamentor.qa.platform.models.dto.user.UserDto;
 import com.javamentor.qa.platform.service.abstracts.dto.user.UserDtoService;
 import javassist.NotFoundException;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class UserDtoServiceImpl implements UserDtoService {
 
     private final UserDtoDao userDtoDao;
 
-    public UserDtoServiceImpl(UserDtoDao userDtoDao) {
-        this.userDtoDao = userDtoDao;
-    }
-
     @Override
-    public Optional<UserDto> getById(Long id) throws NotFoundException {
-        if (userDtoDao.getById(id).isPresent()) {
-            return userDtoDao.getById(id);
+    public UserDto getById(Long id) throws NotFoundException {
+        Optional<UserDto> userDtoOptional = userDtoDao.getById(id);
+        if (userDtoOptional.isPresent()) {
+            return userDtoOptional.get();
         }
 
         throw new NotFoundException("User with id = " + id + " not found");
