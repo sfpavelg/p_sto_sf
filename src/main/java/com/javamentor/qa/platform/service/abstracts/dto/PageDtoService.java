80f17b2657b4cdc7d4560f28363c0bf4cd5e2ee7
alexmcgil <alexmcgil@vivaldi.net>
2023-02-14 21:07:58 +0300

    feat: remove reflection working on catching exceptions

--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/PageDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/dto/PageDtoService.java
@@ -2,23 +2,22 @@ package com.javamentor.qa.platform.service.abstracts.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.PageDtoDao;
 import com.javamentor.qa.platform.models.dto.PageDto;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
 /**
  * Abstract class for creating paginated DTO
  *
- * @param <T>   The type of DTO.
- * @param <Map> Parameters for page selection.
+ * @param <T> The type of DTO.
  */
-public abstract class PageDtoService<T, Map> {
+public abstract class PageDtoService<T> {
 
-    private java.util.Map<String, PageDtoDao<?, ?>> beansMap;
+    private final Map<String, PageDtoDao<T>> beansMap;
 
     @Autowired
-    public void setMap(java.util.Map<String, PageDtoDao<?, ?>> beansMap) {
+    public PageDtoService(Map<String, PageDtoDao<T>> beansMap) {
         this.beansMap = beansMap;
     }
 
@@ -42,34 +41,29 @@ public abstract class PageDtoService<T, Map> {
      *               int currentPageNumber - Specifying the page number to be given
      * @return Page of Dto's
      */
-    protected PageDto<T> PageDto(Map params) {
-        HashMap<?, ?> paramToMap = (HashMap<?, ?>) params;
+    protected PageDto<T> pageDto(Map<String, Object> params) throws NullPointerException, NotFoundException {
         PageDto<T> pageDto = new PageDto<>();
-        String beanName = paramToMap.get("DtoImpl") != null ? (String) paramToMap.get("DtoImpl")
-                : paramToMap.get("DtoType").toString().substring(0, 1).toLowerCase()
-                + paramToMap.get("DtoType").toString().substring(1)
-                + "Impl";
 
-        try {
-            Class<?> BeanClazz = beansMap.get(beanName).getClass();
+        if (beansMap.isEmpty()) {
+            throw new RuntimeException("There are no implementations available in the DAO layer");
+        }
 
-            int totalResultCount = (int) BeanClazz
-                    .getMethod("getTotalResultCount", java.util.Map.class)
-                    .invoke(beansMap.get(beanName), paramToMap);
-            List<T> listItems = (List<T>) BeanClazz
-                    .getMethod("getItems", java.util.Map.class)
-                    .invoke(beansMap.get(beanName), paramToMap);
+        String beanName = params.get("daoDtoImpl") != null ? (String) params.get("daoDtoImpl")
+                : (String) beansMap.keySet().toArray()[0];
 
-            pageDto.setItemsOnPage(paramToMap.get("itemsOnPage") != null ? (int) paramToMap.get("itemsOnPage") : 10);
-            pageDto.setTotalResultCount(totalResultCount);
-            pageDto.setTotalPageCount(paramToMap.get("totalPageCount") != null
-                    ? (int) paramToMap.get("totalPageCount")
-                    : pageDto.getTotalResultCount() / pageDto.getItemsOnPage());
-            pageDto.setCurrentPageNumber(paramToMap.get("currentPageNumber") != null
-                    ? (int) paramToMap.get("currentPageNumber") : 0);
-            pageDto.setItems(listItems);
+        pageDto.setTotalResultCount(beansMap.get(beanName).getTotalResultCount(params));
+        pageDto.setItemsOnPage(params.get("itemsOnPage") != null ? (int) params.get("itemsOnPage") : 10);
+        pageDto.setTotalPageCount(params.get("totalPageCount") != null
+                ? (int) params.get("totalPageCount")
+                : pageDto.getTotalResultCount() / pageDto.getItemsOnPage());
+        pageDto.setCurrentPageNumber(params.get("currentPageNumber") != null
+                ? (int) params.get("currentPageNumber") : 0);
+        pageDto.setItems(beansMap.get(beanName).getItems(params));
 
-        } catch (Exception ignore) {
+        if (pageDto.getTotalResultCount() < 0 | pageDto.getItemsOnPage() < 0
+                | pageDto.getTotalPageCount() < 0 | pageDto.getCurrentPageNumber() < 0
+                | pageDto.getItems().isEmpty()) {
+            throw new NotFoundException("The page with the specified parameters was not found");
         }
 
         return pageDto;
