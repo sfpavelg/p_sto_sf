bdbffedd3849e5a1ef327072e60e7b29d72fdcf4
JeRomeGrill <glushko.r@gmail.com>
2023-02-17 13:56:09 +0300

    fix: moved method to abstract test class, return type changed from header to String (token), mockmvc object deleted from parameters, objectmapper uses instead of json string

--- src/test/java/com/javamentor/qa/platform/AbstractTestApi.java
+++ src/test/java/com/javamentor/qa/platform/AbstractTestApi.java
@@ -3,16 +3,20 @@ package com.javamentor.qa.platform;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.junit5.api.DBRider;
+import com.javamentor.qa.platform.security.jwt.AuthenticationRequest;
 import com.javamentor.qa.platform.webapp.configs.JmApplication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @DBRider
 @SpringBootTest(classes = JmApplication.class)
@@ -23,11 +27,18 @@ public abstract class AbstractTestApi {
 
     @PersistenceContext
     protected EntityManager em;
-//    @Autowired
+    //    @Autowired
 //    protected CacheManager cacheManager;
     @Autowired
     protected MockMvc mvc;
     @Autowired
     protected ObjectMapper objectMapper;
 
+    public String getToken(String email, String password) throws Exception {
+        return mvc.perform(post("http://localhost:8091/api/auth/token")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new AuthenticationRequest(email, password))))
+                .andReturn().getResponse()
+                .getContentAsString().substring(10);
+    }
 }
