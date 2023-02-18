6b6e63c5985c613c627a7b1143630bce5b534e53
alexmcgil <alexmcgil@vivaldi.net>
2023-02-18 20:54:15 +0300

    feat: init ignored and tracked tag data [but however, there is a situation when a user has the same tag that can be ignored and tracked]

--- src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
@@ -1,13 +1,17 @@
 package com.javamentor.qa.platform.service.impl;
 
+import com.javamentor.qa.platform.models.entity.question.IgnoredTag;
 import com.javamentor.qa.platform.models.entity.question.Question;
 import com.javamentor.qa.platform.models.entity.question.Tag;
+import com.javamentor.qa.platform.models.entity.question.TrackedTag;
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.models.entity.user.Role;
 import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
 import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
 import com.javamentor.qa.platform.service.abstracts.model.RoleService;
+import com.javamentor.qa.platform.service.abstracts.model.IgnoredTagService;
+import com.javamentor.qa.platform.service.abstracts.model.TrackedTagService;
 import com.javamentor.qa.platform.service.abstracts.model.UserService;
 import com.javamentor.qa.platform.service.abstracts.model.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,25 +20,32 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 public class TestDataInitService {
     public static Role ROLE_ADMIN = new Role("ROLE_ADMIN");
     public static Role ROLE_USER = new Role("ROLE_USER");
     BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
-    private RoleService roleService;
-    private UserService userService;
-    private QuestionService questionService;
-    private AnswerService answerService;
-    private TagService tagService;
+    private final RoleService roleService;
+    private final UserService userService;
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+    private final TagService tagService;
+    private final IgnoredTagService ignoredTagService;
+    private final  TrackedTagService trackedTagService;
 
     @Autowired
-    public TestDataInitService(RoleService roleService, UserService userService, QuestionService questionService, AnswerService answerService, TagService tagService) {
+    public TestDataInitService(RoleService roleService, UserService userService, QuestionService questionService, AnswerService answerService,
+                               TagService tagService, IgnoredTagService ignoredTagService, TrackedTagService trackedTagService) {
         this.roleService = roleService;
         this.userService = userService;
         this.questionService = questionService;
         this.answerService = answerService;
         this.tagService = tagService;
+        this.ignoredTagService = ignoredTagService;
+        this.trackedTagService = trackedTagService;
     }
 
     public void createSuperUser(int count) {
@@ -124,6 +135,48 @@ public class TestDataInitService {
         tagService.persist(htmlTag);
     }
 
+    public void createIgnoredTags(int ignoredUser) {
+        List<User> userList = userService.getAll();
+        List<Tag> tagList = tagService.getAll();
+
+        for (User user: userList) {
+            if (user.getId() == ignoredUser) {
+                continue;
+            }
+
+            List<Integer> uniqueTags = IntStream.range(0, tagList.size()).boxed().collect(Collectors.toList());
+            Collections.shuffle(uniqueTags);
+
+            for (int ignoredTagId = 0; ignoredTagId < rand(0, 4); ignoredTagId++) {
+                IgnoredTag ignoredTag = new IgnoredTag();
+                ignoredTag.setIgnoredTag(tagList.get(uniqueTags.get(ignoredTagId)));
+                ignoredTag.setUser(user);
+                ignoredTagService.persist(ignoredTag);
+            }
+        }
+    }
+
+    public void createTrackedTags(int ignoredUser) {
+        List<User> userList = userService.getAll();
+        List<Tag> tagList = tagService.getAll();
+
+        for (User user: userList) {
+            if (user.getId() == ignoredUser) {
+                continue;
+            }
+
+            List<Integer> uniqueTags = IntStream.range(0, tagList.size()).boxed().collect(Collectors.toList());
+            Collections.shuffle(uniqueTags);
+
+            for (int trackedTagId = 0; trackedTagId < rand(0, 4); trackedTagId++) {
+                TrackedTag trackedTag = new TrackedTag();
+                trackedTag.setTrackedTag(tagList.get(uniqueTags.get(trackedTagId)));
+                trackedTag.setUser(user);
+                trackedTagService.persist(trackedTag);
+            }
+        }
+    }
+
     public int rand(int leftLimit, int rightLimit) {
         return leftLimit + (int) (Math.random() * rightLimit);
     }
@@ -136,5 +189,7 @@ public class TestDataInitService {
         createTags();
         createQuestion(10);
         createAnswer(50);
+        createIgnoredTags(1);
+        createTrackedTags(1);
     }
 }
