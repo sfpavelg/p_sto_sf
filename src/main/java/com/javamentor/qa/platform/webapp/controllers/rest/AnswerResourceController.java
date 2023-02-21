43d2c2de88b45597ddbe8da59d2355787374f962
vladislavPinigin <112770161+vladislavPinigin@users.noreply.github.com>
2023-02-21 21:56:51 +0300

    fix method_delete

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AnswerResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AnswerResourceController.java
@@ -1,51 +1,42 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
-import com.javamentor.qa.platform.models.dto.answer.AnswerDto;
-import com.javamentor.qa.platform.service.abstracts.dto.answer.AnswerDtoService;
+import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
-import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import javassist.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.List;
-
 @RestController
-@RequestMapping("/api/user/question/{questionId}/answer")
+@RequestMapping("api/user/question/{questionId}/answer")
 @Api("Контроллер для работы с Answer")
 public class AnswerResourceController {
-
-    private final AnswerDtoService answerDtoService;
     private final AnswerService answerService;
 
-    private final QuestionService questionService;
-
-    public AnswerResourceController(AnswerDtoService answerDtoService, AnswerService answerService, QuestionService questionService) {
-        this.answerDtoService = answerDtoService;
+    public AnswerResourceController(AnswerService answerService) {
         this.answerService = answerService;
-        this.questionService = questionService;
     }
 
-    @GetMapping()
-    @ApiOperation(value = "Получение всех элементов AnswerDto по id элемента Question", response = AnswerDto.class)
+
+    @ApiOperation(value = "Удаление ответа ")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Success request. AnswerDto objects returned in response (May be empty list)"),
-            @ApiResponse(code = 401, message = "Unauthorized request"),
-            @ApiResponse(code = 403, message = "Forbidden"),
-            @ApiResponse(code = 404, message = "No question with such id")})
-    public ResponseEntity<?> getAllByQuestionId(@PathVariable("questionId") Long questionId) throws NotFoundException {
-        if (!questionService.existsById(questionId)) {
-            return new ResponseEntity<>("No question with such id", HttpStatus.NOT_FOUND);
+            @ApiResponse(code = 400, message = "Ответ с указанным уникальным идентификатором (Id) не найден")
+    })
+    @DeleteMapping("/{answerId}")
+    public ResponseEntity<Void> deleteAnswer(@PathVariable Long answerId) {
+        if (answerService.getById(answerId).isPresent()) {
+            return new ResponseEntity<>(HttpStatus.OK);
         }
-        return new ResponseEntity<>(answerDtoService.getAllByQuestionId(questionId), HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
     }
 }
+
+
--- src/test/java/com/javamentor/qa/platform/api/TestAnswerResourceController.java
+++ src/test/java/com/javamentor/qa/platform/api/TestAnswerResourceController.java
@@ -16,7 +16,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-
 public class TestAnswerResourceController extends AbstractTestApi {
 
     @Test
@@ -26,8 +25,12 @@ public class TestAnswerResourceController extends AbstractTestApi {
             @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
                     value = {"/script/TestAnswerResourceController/testGetAllByQuestionId/After.sql"})
     })
-    public void testGetAllByQuestionId() throws Exception {
-        String token = getToken("0@gmail.com", "0pwd");
+    public void testDeleteAnswer() throws Exception {
+
+        String json = "{\"email\":\"0@gmail.com\",\"password\":\"0pwd\"}";
+        String token = this.mvc.perform(post("/api/auth/token").contentType(MediaType.APPLICATION_JSON)
+                        .content(json)).andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString().substring(10);
 
         //success
         this.mvc.perform(get("/api/user/question/{id}/answer", 100).header("Authorization", "Bearer " + token))
@@ -45,22 +48,6 @@ public class TestAnswerResourceController extends AbstractTestApi {
                 .andExpect(jsonPath("$[0].countValuable", Is.is(0)))
                 .andExpect(jsonPath("$[0].image", Is.is("http://imagelink1.com")))
                 .andExpect(jsonPath("$[0].nickName", Is.is("nickname1")));
-        //success (with votes on answer)
-        this.mvc.perform(get("/api/user/question/{id}/answer", 102).header("Authorization", "Bearer " + token))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].id", Is.is(102)))
-                .andExpect(jsonPath("$[0].userId", Is.is(104)))
-                .andExpect(jsonPath("$[0].userReputation", Is.is(0)))
-                .andExpect(jsonPath("$[0].questionId", Is.is(102)))
-                .andExpect(jsonPath("$[0].body", Is.is("html_body3")))
-                .andExpect(jsonPath("$[0].persistDate", Is.is("2023-01-27T13:01:11.245126")))
-                .andExpect(jsonPath("$[0].isHelpful", Is.is(true)))
-                .andExpect(jsonPath("$[0].dateAccept", Is.is("2023-01-27T13:01:11.245126")))
-                .andExpect(jsonPath("$[0].countValuable", Is.is(2)))
-                .andExpect(jsonPath("$[0].image", Is.is("http://imagelink5.com")))
-                .andExpect(jsonPath("$[0].nickName", Is.is("nickname5")));
 
         //question with no answers
         this.mvc.perform(get("/api/user/question/{id}/answer", 103).header("Authorization", "Bearer " + token))
@@ -74,22 +61,22 @@ public class TestAnswerResourceController extends AbstractTestApi {
                 .andDo(print())
                 .andExpect(status().isNotFound());
 
-        //null results in DB (only possible fields like imageLink, rep and valuable)
-        this.mvc.perform(get("/api/user/question/{id}/answer", 104).header("Authorization", "Bearer " + token))
+        //null results in DB (only possible fields like imageLink, rep, counts of answers and valuable)
+        this.mvc.perform(get("/api/user/question/{id}/answer", 102).header("Authorization", "Bearer " + token))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].id", Is.is(100)))
-                .andExpect(jsonPath("$[0].userId", Is.is(102)))
-                .andExpect(jsonPath("$[0].userReputation", Is.is(5)))
-                .andExpect(jsonPath("$[0].questionId", Is.is(104)))
-                .andExpect(jsonPath("$[0].body", Is.is("html_body1")))
+                .andExpect(jsonPath("$[0].id", Is.is(102)))
+                .andExpect(jsonPath("$[0].userId", Is.is(104)))
+                .andExpect(jsonPath("$[0].userReputation", Is.is(0)))
+                .andExpect(jsonPath("$[0].questionId", Is.is(102)))
+                .andExpect(jsonPath("$[0].body", Is.is("html_body3")))
                 .andExpect(jsonPath("$[0].persistDate", Is.is("2023-01-27T13:01:11.245126")))
                 .andExpect(jsonPath("$[0].isHelpful", Is.is(true)))
                 .andExpect(jsonPath("$[0].dateAccept", Is.is("2023-01-27T13:01:11.245126")))
                 .andExpect(jsonPath("$[0].countValuable", Is.is(0)))
                 .andExpect(jsonPath("$[0].image", IsNull.nullValue()))
-                .andExpect(jsonPath("$[0].nickName", Is.is("nickname3")));
+                .andExpect(jsonPath("$[0].nickName", Is.is("nickname5")));
     }
 
 }
