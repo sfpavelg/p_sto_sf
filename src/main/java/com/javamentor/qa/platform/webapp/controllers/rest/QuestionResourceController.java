15db3e8ba65dc4b5f8f50c44ed5808eaea9f2530
tayalyz <107634713+tayalyz@users.noreply.github.com>
2023-03-03 17:53:57 +0300

    annotations fix

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
@@ -13,7 +13,12 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import javassist.NotFoundException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
 
