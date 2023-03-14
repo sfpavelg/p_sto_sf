65b928f2e79bcf04e0239a71e63311eccdb8ce41
Семин Дмитрий <hitman101086@gmail.com>
2023-03-14 21:29:41 +0300

    Исправлено получение текущего пользователя в контроллере

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/TagResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/TagResourceController.java
@@ -12,9 +12,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import javassist.NotFoundException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,13 +32,11 @@ public class TagResourceController {
 
     private final TrackedTagService trackedTagService;
 
-    private final UserService userService;
 
-    public TagResourceController(TagDtoService tagDtoService, IgnoredTagService ignoredTagService, TrackedTagService trackedTagService, UserService userService) {
+    public TagResourceController(TagDtoService tagDtoService, IgnoredTagService ignoredTagService, TrackedTagService trackedTagService) {
         this.tagDtoService = tagDtoService;
         this.ignoredTagService = ignoredTagService;
         this.trackedTagService = trackedTagService;
-        this.userService = userService;
     }
 
     @GetMapping("/related")
@@ -103,9 +99,9 @@ public class TagResourceController {
      */
     @GetMapping("/tracked")
     @ApiOperation(value = "Getting all TrackedTags of an authorized user", response = List.class)
-    public ResponseEntity<List<TagDto>> getAllTrackedTagAuthenticatedUser(@AuthenticationPrincipal UserDetails authenticatedUser) {
-        Long idAuthenticatedUser = userService.getByEmail(authenticatedUser.getUsername()).orElseThrow().getId();
-        return ResponseEntity.ok(tagDtoService.getTrackedTagsByUserId(idAuthenticatedUser));
+    public ResponseEntity<List<TagDto>> getAllTrackedTagAuthenticatedUser() {
+        User authenticatedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok(tagDtoService.getTrackedTagsByUserId(authenticatedUser.getId()));
     }
 
 }
