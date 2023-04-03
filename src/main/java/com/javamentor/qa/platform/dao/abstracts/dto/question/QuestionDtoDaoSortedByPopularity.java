dbc9e982081bddbd1f28bd32dd818254ca730e1d
Pavel Sofein <sfpavelg@gmail.com>
2023-04-03 14:11:36 +0300

    commit #2  codestyle feat: added questions controller sorted by Popularity feat: added test

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/question/QuestionDtoDaoSortedByPopularity.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/question/QuestionDtoDaoSortedByPopularity.java
@@ -3,5 +3,5 @@ package com.javamentor.qa.platform.dao.abstracts.dto.question;
 import com.javamentor.qa.platform.dao.abstracts.dto.PageDtoDao;
 import com.javamentor.qa.platform.models.dto.question.QuestionDto;
 
-public interface QuestionDtoDaoSortedByPopularity  extends PageDtoDao<QuestionDto> {
+public interface QuestionDtoDaoSortedByPopularity extends PageDtoDao<QuestionDto> {
 }
