10e2dfb70cad3ab944cdf8b5d6e76d67c168ed74
Ruslan <ruslanzel@list.ru>
2023-04-07 14:05:23 +0300

    Delete beansMap from TagDtoServiceImpl

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/tag/TagDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/tag/TagDtoServiceImpl.java
@@ -1,24 +1,20 @@
 package com.javamentor.qa.platform.service.impl.dto.tag;
 
-import com.javamentor.qa.platform.dao.abstracts.dto.PageDtoDao;
 import com.javamentor.qa.platform.dao.abstracts.dto.tag.TagDtoDao;
 import com.javamentor.qa.platform.models.dto.tag.RelatedTagsDto;
 import com.javamentor.qa.platform.models.dto.tag.TagDto;
-import com.javamentor.qa.platform.service.abstracts.dto.PageDtoService;
 import com.javamentor.qa.platform.service.abstracts.dto.tag.TagDtoService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
-public class TagDtoServiceImpl extends PageDtoService<TagDto> implements TagDtoService {
+public class TagDtoServiceImpl implements TagDtoService {
 
     private final TagDtoDao tagDtoDao;
 
-    public TagDtoServiceImpl(TagDtoDao tagDtoDao, Map<String, PageDtoDao<TagDto>> beansMap) {
-        super(beansMap);
+    public TagDtoServiceImpl(TagDtoDao tagDtoDao) {
         this.tagDtoDao = tagDtoDao;
     }
 
