f162f25cdf17b0c306708345f39586431cbdcb5b
Mannelig <mannelig92@gmail.com>
2023-04-26 00:06:59 +0300

    v1.2

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/QuestionResourceController.java
@@ -25,8 +25,6 @@ import javassist.NotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
