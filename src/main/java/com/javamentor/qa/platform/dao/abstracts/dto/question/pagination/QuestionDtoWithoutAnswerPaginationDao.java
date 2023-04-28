1eab15bd1f135e15047fc113783ec490715c50d9
bborisovspb <Kelt87@GMail.com>
2023-04-28 16:23:29 +0300

    Delete unused import

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/question/pagination/QuestionDtoWithoutAnswerPaginationDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/question/pagination/QuestionDtoWithoutAnswerPaginationDao.java
@@ -2,10 +2,6 @@ package com.javamentor.qa.platform.dao.abstracts.dto.question.pagination;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.PageDtoDao;
 import com.javamentor.qa.platform.models.dto.question.QuestionViewDto;
-import com.javamentor.qa.platform.models.dto.tag.TagDto;
-
-import java.util.List;
-import java.util.Map;
 
 public interface QuestionDtoWithoutAnswerPaginationDao  extends PageDtoDao<QuestionViewDto> {
 
