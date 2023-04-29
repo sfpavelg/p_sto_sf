b1f08e9e17dd0b469829591b5d2ec74c37450e0d
bborisovspb <Kelt87@GMail.com>
2023-04-29 17:06:02 +0300

    Delete unused imports etc

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/UserResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/UserResourceController.java
@@ -13,7 +13,6 @@ import javassist.NotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,7 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
