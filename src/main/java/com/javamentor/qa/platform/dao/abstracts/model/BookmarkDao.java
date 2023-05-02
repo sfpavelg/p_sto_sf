3111ebee18d2fec518a91c7f26a39eebf459bd49
Ap4u11 <artyom-panfilov@yandex.ru>
2023-05-02 19:57:24 +0300

    initial commit initial Chat,SingleChat,GroupChat and Message

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/model/BookmarkDao.java
+++
@@ -1,7 +0,0 @@
-package com.javamentor.qa.platform.dao.abstracts.model;
-
-import com.javamentor.qa.platform.models.entity.BookMarks;
-
-public interface BookmarkDao extends ReadWriteDao<BookMarks, Long>{
-    boolean duplicateQuestionCheck(Long userId, Long questionId);
-}
--- src/main/java/com/javamentor/qa/platform/service/abstracts/model/BookmarkService.java
+++
@@ -1,9 +0,0 @@
-package com.javamentor.qa.platform.service.abstracts.model;
-
-import com.javamentor.qa.platform.models.entity.BookMarks;
-import com.javamentor.qa.platform.models.entity.user.User;
-import javassist.NotFoundException;
-
-public interface BookmarkService extends ReadWriteService<BookMarks, Long>{
-    BookMarks persistByQuestionId(Long id, User user) throws NotFoundException;
-}
