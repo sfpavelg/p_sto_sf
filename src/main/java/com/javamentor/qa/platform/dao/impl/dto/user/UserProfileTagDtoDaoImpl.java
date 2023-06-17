beb8ed5153927fe655dc293a9612b4fff98cff18
Alexei <splash.dll@gmail.com>
2023-06-17 14:51:36 +0300

    2 fields

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/user/UserProfileTagDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/user/UserProfileTagDtoDaoImpl.java
@@ -2,11 +2,12 @@ package com.javamentor.qa.platform.dao.impl.dto.user;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.user.UserProfileTagDtoDao;
 import com.javamentor.qa.platform.models.dto.user.UserProfileTagDto;
+import com.javamentor.qa.platform.models.entity.question.Tag;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -41,11 +42,57 @@ public class UserProfileTagDtoDaoImpl implements UserProfileTagDtoDao {
 //                "GROUP BY t.name ", UserProfileTagDto.class).setParameter("userId", userId);
 //List <UserProfileTagDto>list=query1.getResultList();
 //        list.stream().forEach(userProfileTagDto -> System.out.println(userProfileTagDto));
+//
+//        Query query = entityManager.createQuery("SELECT new com.javamentor.qa.platform.models.dto.user.UserProfileTagDto( " +
+//                " t.name) " +
+//                "FROM Tag t " +
+//                "JOIN Question.tags q " +
+//                "ON q.id = t.id " +
+//                "WHERE q.id = :userId ", UserProfileTagDto.class).setParameter("userId", userId);
 
-        Query query = entityManager.createQuery("SELECT new com.javamentor.qa.platform.models.dto.user.UserProfileTagDto( " +
-                " t.name) " +
-                "FROM Tag t " +
-                "WHERE t.id = :userId ", UserProfileTagDto.class).setParameter("userId", userId);
+        Query query = entityManager.createNativeQuery("SELECT tag.name as tagName, COUNT(tag_id) AS countVoteTag " +
+                "FROM question_has_tag" +
+                "         JOIN tag " +
+                "              ON question_has_tag.tag_id = tag.id " +
+                "WHERE question_id IN (" +
+                "    SELECT id FROM question WHERE user_id = ? " +
+                "    UNION " +
+                "    SELECT question_id FROM answer WHERE user_id = ?" +
+                ")" +
+                "GROUP BY tag.name;").setParameter(1, userId).setParameter(2, userId);
+        List<Object[]> lst = (List<Object[]>) query.getResultList();
+        List<UserProfileTagDto>  result = new ArrayList<>();
+        for (int i = 0; i < lst.size(); i++) {
+           Object[] o = lst.get(i);
+           result.add(new UserProfileTagDto(o[0].toString(), Long.valueOf(o[1].toString())));
+        }
+
+
+
+//        for (Object[] u : lst) {
+//            System.out.println(u);
+//        }
+//        List <UserProfileTagDto[]> dtoList =( List <UserProfileTagDto[]>) query.getResultList();
+//for (UserProfileTagDto[] tagDtos : dtoList) {
+//    System.out.println(tagDtos);
+//}
+
+
+//        Query query = entityManager.createNativeQuery("SELECT * " +
+//                "FROM question_has_tag " +
+//                "         JOIN tag " +
+//                "              ON question_has_tag.tag_id = tag.id " +
+//                "WHERE question_id IN ( " +
+//                "    SELECT id FROM question WHERE user_id = :userId " +
+//                "    UNION " +
+//                "    SELECT question_id FROM answer WHERE user_id = :userId " +
+//                ")" +
+//                ";", Tag.class).setParameter("userId", userId);
+//        List<Tag> lst = query.getResultList();
+//        for (Tag u : lst) {
+//            System.out.println(u);
+//        }
+
 //        select tag.name,
 //                concat(count(case when votes_on_answers.vote = 'UP_VOTE' then 1 end) - count(case when votes_on_answers.vote = 'DOWN_VOTE' then 1 end)) as up_votes
 //        from votes_on_answers
@@ -58,6 +105,6 @@ public class UserProfileTagDtoDaoImpl implements UserProfileTagDtoDao {
 //        where votes_on_answers.user_id = 1
 //        group by tag.name
 
-        return query.getResultList();
+        return result;
     }
 }
