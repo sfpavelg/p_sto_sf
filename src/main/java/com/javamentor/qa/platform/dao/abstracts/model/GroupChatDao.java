6eee571a793f626c97ce917ebdd265d53073191e
Your Name <Dio555meister@gmail.com>
2023-07-13 13:40:56 +0300

    5.0

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/model/GroupChatDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/model/GroupChatDao.java
@@ -2,7 +2,6 @@ package com.javamentor.qa.platform.dao.abstracts.model;
 
 import com.javamentor.qa.platform.models.entity.chat.GroupChat;
 import com.javamentor.qa.platform.models.entity.user.User;
-import javassist.NotFoundException;
 
 import java.util.Set;
 
--- src/main/java/com/javamentor/qa/platform/dao/impl/model/GroupChatDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/GroupChatDaoImpl.java
@@ -3,7 +3,6 @@ package com.javamentor.qa.platform.dao.impl.model;
 import com.javamentor.qa.platform.dao.abstracts.model.GroupChatDao;
 import com.javamentor.qa.platform.models.entity.chat.GroupChat;
 import com.javamentor.qa.platform.models.entity.user.User;
-import javassist.NotFoundException;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
