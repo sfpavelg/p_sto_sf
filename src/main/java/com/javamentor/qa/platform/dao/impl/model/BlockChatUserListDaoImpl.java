9ebd7080d7f99d58868bf10addb21b8b833a5d17
antlau2000 <antlau2000@yandex.ru>
2023-07-21 17:21:12 +0300

    add save constraint in BlockChatUserListDaoImpl

--- src/main/java/com/javamentor/qa/platform/dao/impl/model/BlockChatUserListDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/model/BlockChatUserListDaoImpl.java
@@ -4,6 +4,25 @@ import com.javamentor.qa.platform.dao.abstracts.model.BlockChatUserListDao;
 import com.javamentor.qa.platform.models.entity.user.BlockChatUserList;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Repository
 public class BlockChatUserListDaoImpl extends ReadWriteDaoImpl<BlockChatUserList, Long> implements BlockChatUserListDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void persist(BlockChatUserList blockChatUserList) {
+        BlockChatUserList existingBlock = entityManager.createQuery(
+                        "SELECT b FROM BlockChatUserList b WHERE b.profile = :profile AND b.blocked = :blocked",
+                        BlockChatUserList.class)
+                .setParameter("profile", blockChatUserList.getProfile())
+                .setParameter("blocked", blockChatUserList.getBlocked())
+                .getResultList().stream().findFirst().orElse(null);
+        if (existingBlock == null) {
+            entityManager.persist(blockChatUserList);
+        }
+    }
 }
