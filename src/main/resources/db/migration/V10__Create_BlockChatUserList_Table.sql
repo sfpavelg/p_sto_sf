7ce6ef8e88abdfe060543b92e2d1979216d2281c
antlau2000 <antlau2000@yandex.ru>
2023-07-31 23:30:02 +0300

    create sequence block_chat_user_list_id_seq

--- src/main/resources/db/migration/V10__Create_BlockChatUserList_Table.sql
+++ src/main/resources/db/migration/V10__Create_BlockChatUserList_Table.sql
@@ -7,3 +7,10 @@ create table if not exists "block_chat_user_list"
     foreign key (profile_id) references user_entity (id),
     foreign key (blocked_id) references user_entity (id)
 );
+
+create sequence if not exists block_chat_user_list_id_seq
+    start with 1
+    increment by 1
+    no minvalue
+    no maxvalue
+    cache 1;
